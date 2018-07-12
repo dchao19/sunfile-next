205a3852359a5755835db84139bdce931d91883e
Daniel Chao <daniel@danielchao.me>
2018-07-11 21:31:41 -0600

    Cleanup statusCheck func

--- functions/src/api/statusCheck/statusCheck.func.ts
+++ functions/src/api/statusCheck/statusCheck.func.ts
@@ -1,5 +1,4 @@
 import { Context, HttpRequest, HttpStatusCode } from "azure-functions-ts-essentials";
-import { sequelize } from "../../setups/dbSetup";
 
 export async function run(context: Context, req: HttpRequest): Promise<void> {
     context.log("api/ping received a request");
