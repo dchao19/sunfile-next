ea74b4a0f84cc94d96f62bf07e6b2f9ba25cf096
Daniel Chao <daniel@danielchao.me>
2018-07-11 21:30:54 -0600

    Remove db logic from statuscheck func

--- functions/src/api/statusCheck/statusCheck.func.ts
+++ functions/src/api/statusCheck/statusCheck.func.ts
@@ -2,22 +2,12 @@ import { Context, HttpRequest, HttpStatusCode } from "azure-functions-ts-essenti
 import { sequelize } from "../../setups/dbSetup";
 
 export async function run(context: Context, req: HttpRequest): Promise<void> {
-    context.log("TypeScript HTTP trigger function processed a request.");
+    context.log("api/ping received a request");
 
-    await sequelize.sync();
-    try {
-        await sequelize.authenticate();
-
-        context.res = {
-            status: HttpStatusCode.OK,
-            body: "pong"
-        };
-    } catch (e) {
-        context.res = {
-            status: HttpStatusCode.InternalServerError,
-            body: null
-        };
-    }
+    context.res = {
+        status: HttpStatusCode.OK,
+        body: "pong"
+    };
 
     context.done();
 }
